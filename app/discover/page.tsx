780b5cf38a7b6785b21eb825604d2ed8f0073b62
asafamz <asafamshop@gmail.com>
2026-09-09 18:05:28 +0300

    Delete app directory

--- app/discover/page.tsx
+++
@@ -1,1 +0,0 @@
-export default function DiscoverPage(){return <main className="shell"><div className="card centered"><h1>Asafamz Digital Library</h1><p className="desc">Digital products for Asafamz customers.</p></div></main>}
